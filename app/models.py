cd6bd36427a2c84995f3b0082e9d85dac42a202e
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-29 12:38:15 +0300

    create Location class in models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        
+        return self.name
+    
+    
+    
