0021b1d4a0ebcb9a590fc5172ec46826d755b9c4
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-29 13:25:50 +0300

    create index view and url

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login')
+def index(request):
+    user = request.user
+    
+    try:
+        profile = Profile.objects.get(user = user)
+        
+    except:
+        profile = Profile.objects.create(user = user, name = user.username)
+        profile.save()
+        
+    posts = Post.objects.filter(neighbourhood = profile.neighbourhood)
+    businesses = Business.objects.filter(neighbourhood = profile.neighbourhood)
+    neighbourhood = profile.neighbourhood
+    
+    return render(request, 'index.html', {"posts": posts, "businesses": businesses, "neighbourhood": neighbourhood})
