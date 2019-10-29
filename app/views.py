1e9f492d3ae559697b5c9781aa9e4c9f1b673f8e
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-29 13:44:54 +0300

    create business search view

--- app/views.py
+++ app/views.py
@@ -8,8 +8,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/accounts/login')
 def index(request):
-    user = request.user
     
+    user = request.user
     try:
         profile = Profile.objects.get(user = user)
         
@@ -22,3 +22,22 @@ def index(request):
     neighbourhood = profile.neighbourhood
     
     return render(request, 'index.html', {"posts": posts, "businesses": businesses, "neighbourhood": neighbourhood})
+
+
+@login_required(login_url='/accounts/login')
+def search_results(request):
+
+    if 'business' in request.GET and request.GET["business"]:
+        
+        profile = Profile.objects.get(user = request.user)
+        search_term = request.GET.get("business")
+        searched_businesses = Business.objects.filter(neighbourhood = profile.neighbourhood, name__icontains = search_term)
+        
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "businesses": searched_businesss})
+
+    else:
+        message = "You haven't searched for any business"
+        
+        return render(request, 'search.html', {"message": message})
