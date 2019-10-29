668ee2b64fff19b71c9f863f650e7073d95a82a7
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-29 12:51:10 +0300

    create Neighbourhood class in models

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Location(models.Model):
+    
     name = models.CharField(max_length = 30)
 
     def __str__(self):
@@ -13,3 +14,13 @@ class Location(models.Model):
     
     
     
+class Neighbourhood(models.Model):
+    
+    name = models.CharField(max_length=30)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants = models.IntegerField(null=True, default=0)
+    
+    
+    def __str__(self):
+        
+        return self.name
